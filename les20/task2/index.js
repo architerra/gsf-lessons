8d4881aef7d62632a22072cb7dae4e1937d5faf5
Olexiy Voedylo <architerr@gmail.com>
2022-11-02 13:32:10 +0200

    ch2

--- les20/task2/index.js
+++ les20/task2/index.js
@@ -1,10 +1,10 @@
-function getTitleElement() {
+export function getTitleElement() {
   let titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 }
 
-function getInputElement() {
+export function getInputElement() {
   let inputElem = document.querySelector('[type="text"]');
   console.dir(inputElem);
   return inputElem;
