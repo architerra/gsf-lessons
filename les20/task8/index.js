6f7842c49f53477bde96d840bfccf13a958edc28
Olexiy Voedylo <architerr@gmail.com>
2022-11-02 18:35:26 +0200

    ch5

--- les20/task8/index.js
+++ les20/task8/index.js
@@ -1,5 +1,6 @@
 export function createButton(buttonText) {
+  let bodyElem = document.querySelector('body');
   const element = document.createElement('button');
   element.textContent = buttonText;
-  body.append(element);
+  bodyElem.append(element);
 }
