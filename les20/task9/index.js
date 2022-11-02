6de032ff369bb53ba08a0fe67486fae3342d6285
Olexiy Voedylo <architerr@gmail.com>
2022-11-02 19:29:06 +0200

    ch5

---
+++ les20/task9/index.js
@@ -0,0 +1,26 @@
+export function finishList() {
+  //   let bodyElem = document.querySelector('body');
+  //   const element = document.createElement('button');
+  //   element.textContent = buttonText;
+  //   bodyElem.append(element);
+
+  let ulElem = document.querySelector('.list');
+
+  const liElemOne = document.createElement('li');
+  liElemOne.textContent = '1';
+  ulElem.prepend(liElemOne);
+
+  const liElemEight = document.createElement('li');
+  liElemEight.textContent = '8';
+  ulElem.prepend(liElemEight);
+
+  let specElem = document.querySelector('.special');
+
+  const liElemFour = document.createElement('li');
+  liElemFour.textContent = '4';
+  specElem.before(liElemFour);
+
+  const liElemSix = document.createElement('li');
+  liElemSix.textContent = '6';
+  specElem.after(liElemSix);
+}
