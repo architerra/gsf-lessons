6a2b6e51fcfbeef6dfa6ba3209306bf91c0c7c9b
Olexiy Voedylo <architerr@gmail.com>
2022-11-02 19:01:50 +0200

    ch6

--- les20/task14/index.js
+++ les20/task14/index.js
@@ -1,3 +1,5 @@
 export function getSection(num) {
   let spanElem = document.querySelector(`span[data-number="${num}"]`);
+  let boxElem = spanElem.closest('.box');
+  return boxElem.getAttribute('data-section');
 }
