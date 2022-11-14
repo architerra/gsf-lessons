d2f2b4871a9157bf7df71aabcda36285c5ad1e19
Olexiy Voedylo <architerr@gmail.com>
2022-11-14 14:59:51 +0200

    ch1

---
+++ les24/task1/index.js
@@ -0,0 +1,7 @@
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+export function dayOfWeek(date, days) {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDays[new Date(dateInFuture).getDay()];
+}
