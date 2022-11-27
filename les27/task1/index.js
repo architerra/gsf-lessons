754c573343d7f25c44eb55b225ff30c06e0509a0
Olexiy Voedylo <architerr@gmail.com>
2022-11-27 10:59:34 +0200

    ch3

--- les27/task1/index.js
+++ les27/task1/index.js
@@ -1,14 +1,19 @@
+// localStorage.setItem('Hobbies', JSON.stringify({ name: Tom }));
+// localStorage.setItem('name', 'Tom');
+
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    let newValue;
-    try {
-      newValue = JSON.parse(value);
-    } catch (e) {
-      newValue = value;
-    }
+    // let newValue;
+    // try {
+    //   newValue = JSON.parse(value);
+    // } catch (e) {
+    //   newValue = value;
+    // }
     return {
       ...acc,
-      [key]: JSON.parse(newValue),
+      [key]: JSON.parse(value),
     };
   }, {});
 };
+
+// console.log(getLocalStorageData());
