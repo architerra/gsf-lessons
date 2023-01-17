138206a3187d925a468c46f363243807145bb5f3
Olexiy Voedylo <architerr@gmail.com>
2023-01-17 15:20:25 +0200

    ch3

--- les29/task5/index.js
+++ les29/task5/index.js
@@ -1,59 +1,59 @@
 export const shmoment = initDate => {
-    let result = new Date(initDate).getTime();
+    let resultDate = new Date(initDate).getTime();
 
     const calculator = {
         add(timeInterval, number) {
             if (timeInterval === 'years') {
-                result += (number * 31556952000);
+                resultDate += (number * 31556952000);
             }
             if (timeInterval === 'months') {
-                result += (number * 2629746000);
+                resultDate += (number * 2629746000);
             }
             if (timeInterval === 'days') {
-                result += (number * 86400000);
+                resultDate += (number * 86400000);
             }
             if (timeInterval === 'hours') {
-                result += (number * 3600000);
+                resultDate += (number * 3600000);
             }
             if (timeInterval === 'minutes') {
-                result += (number * 60000);
+                resultDate += (number * 60000);
             }
             if (timeInterval === 'seconds') {
-                result += (number * 1000);
+                resultDate += (number * 1000);
             }
             if (timeInterval === 'milliseconds') {
-                result += number;
+                resultDate += number;
             };
             return this;
         },
        
         subtract(timeInterval, number) {
             if (timeInterval === 'years') {
-                result -= (number * 31556952000);
+                resultDate -= (number * 31556952000);
             }
             if (timeInterval === 'months') {
-                result -= (number * 2629746000);
+                resultDate -= (number * 2629746000);
             }
             if (timeInterval === 'days') {
-                result -= (number * 86400000);
+                resultDate -= (number * 86400000);
             }
             if (timeInterval === 'hours') {
-                result -= (number * 3600000);
+                resultDate -= (number * 3600000);
             }
             if (timeInterval === 'minutes') {
-                result -= (number * 60000);
+                resultDate -= (number * 60000);
             }
             if (timeInterval === 'seconds') {
-                result -= (number * 1000);
+                resultDate -= (number * 1000);
             }
             if (timeInterval === 'milliseconds') {
-                result -= number;
+                resultDate -= number;
             };
             return this;
         },
         
         result() {
-            return result.toString;
+            return resultDate.toString;
         }
     };
 
