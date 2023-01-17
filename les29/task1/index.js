45949a901da331667cdf896f03054bdf123e0f4c
Olexiy Voedylo <architerr@gmail.com>
2023-01-17 11:32:51 +0200

    ch1

---
+++ les29/task1/index.js
@@ -0,0 +1,25 @@
+const calc = initValue => {
+    let result = initValue;
+
+    const calculator = {
+        add(value) {
+            result += value;
+            return this;
+        },
+        mult(value) {
+            result *= value;
+            return this;
+        },
+        substract(value) {
+            result -= value;
+            return this;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result;
+        }
+    }
+}
