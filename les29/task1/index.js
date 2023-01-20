21bdb40b911a0129215216051186df834dae3e09
Olexiy Voedylo <architerr@gmail.com>
2023-01-20 12:52:00 +0200

    ch3

--- les29/task1/index.js
+++ les29/task1/index.js
@@ -1,4 +1,4 @@
-export const addImage = (imgSrc, callback) => {
+const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
   imgElem.setAttribute('alt', 'My Photo');
   imgElem.src = imgSrc;
@@ -15,21 +15,19 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-// callack function
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, { width, height }) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
 };
 
 // examples
-// addImage(
-//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//   onImageLoaded,
-// );
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
