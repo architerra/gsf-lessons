6976d0ff5add4bba138054b06c3aa67c2396abbe
Olexiy Voedylo <architerr@gmail.com>
2023-01-20 13:15:59 +0200

    ch7

--- les29/task1/index.js
+++ les29/task1/index.js
@@ -15,18 +15,18 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-export const onImageLoaded = (error, { width, height }) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
 
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-// examples
 // addImage(
 //   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
 //   onImageLoaded,
