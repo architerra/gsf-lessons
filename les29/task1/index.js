44b42fcc961aee0d6652dded9a70de09fd6f9154
Olexiy Voedylo <architerr@gmail.com>
2023-01-20 12:44:01 +0200

    ch2

--- les29/task1/index.js
+++ les29/task1/index.js
@@ -1,22 +1,35 @@
-const addImage = (imgSrc, callback) => {
-    
-  };
-  
-  // callack function
-  const onImageLoaded = (error, imgElem) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-  
+export const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
     const { width, height } = imgElem;
-    const sizeElem = document.querySelector('.image-size');
-  
-    sizeElem.textContent = `${width} x ${height}`;
+    callback(null, { width, height });
   };
-  
-  // examples
-  // addImage(
-  //   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  //   onImageLoaded,
-  // );
+
+  imgElem.addEventListener('load', onImageLoaded);
+
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
+};
+
+// callack function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
