3458fb103bff79ce439589f6143a9c5f65fc0b93
Olexiy Voedylo <architerr@gmail.com>
2023-01-22 12:15:11 +0200

    ch1

---
+++ les30/task1/index.js
@@ -0,0 +1,20 @@
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+
+  return p;
+};
