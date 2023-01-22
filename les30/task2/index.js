de4ef69ab051e3c031f3fcc084c6e12569be2861
Olexiy Voedylo <architerr@gmail.com>
2023-01-22 12:34:04 +0200

    ch4

--- les30/task2/index.js
+++ les30/task2/index.js
@@ -22,6 +22,6 @@ export const addImageV2 = url => {
 
 // examples
 
-// addImageV2('https://server.com/image.png')
-//   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-//   .catch(error => console.log(error)); // ==> 'Image load failed'
+addImageV2('https://server.com/image.png')
+  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch(error => console.log(error)); // ==> 'Image load failed'
