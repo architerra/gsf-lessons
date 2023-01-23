5a4d1937f3010456e27108f84b6ad823e42ed5f6
Olexiy Voedylo <architerr@gmail.com>
2023-01-23 20:45:40 +0200

    ch1

---
+++ les32/task1/index.js
@@ -0,0 +1,8 @@
+const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((acc, num) => acc + Number(num), 0);
+
+export const asyncSum = (...asyncNumbers) => {
+  return Promise.all(asyncNumbers)
+    .then(numbers => getSum(numbers))
+    .catch(() => Promise.reject(new Error("Can't calculat")));
+};
