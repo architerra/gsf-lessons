0088ece1fddc93c1c4ba34205ad5161ba62194f3
Olexiy Voedylo <architerr@gmail.com>
2023-02-15 11:08:41 +0200

    ch2

--- les36/task2/gateways.js
+++ les36/task2/gateways.js
@@ -1,16 +1,15 @@
-export const fetchUserData = userName => {
-  return fetch(`https://api.github.com/users/${userName}`).then(responce => {
-    if (responce.ok) {
-      return responce.json();
-    }
-    throw new Error('Failed to load data');
-  });
+export const fetchUserData = async userName => {
+  const responce = await fetch(`https://api.github.com/users/${userName}`);
+  if (responce.ok) {
+    return await responce.json();
+  }
+  throw new Error('Failed to load data');
 };
 
-export const fetchRepositories = url =>
-  fetch(url).then(responce => {
-    if (responce.ok) {
-      return responce.json();
-    }
-    throw new Error('Failed to load data');
-  });
+export const fetchRepositories = async url => {
+  const responce = await fetch(url);
+  if (responce.ok) {
+    return await responce.json();
+  }
+  throw new Error('Failed to load data');
+};
