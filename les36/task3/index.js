8c654b93b2ab05bdc49bdf8e1142febd44285c83
Olexiy Voedylo <architerr@gmail.com>
2023-02-15 17:52:25 +0200

    ch2

---
+++ les36/task3/index.js
@@ -0,0 +1,19 @@
+const fetchUserData = async userName => {
+  const responce = await fetch(`https://api.github.com/users/${userName}`);
+  //   console.log(responce);
+  return responce.json();
+};
+
+export const getUsersBlogs = users => {
+  const result = [];
+  users.forEach(e => {
+    const userData = fetchUserData(e);
+    result.push(userData);
+  });
+  //   console.log(result);
+  return Promise.all(result);
+};
+
+// examples
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']); //.then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
